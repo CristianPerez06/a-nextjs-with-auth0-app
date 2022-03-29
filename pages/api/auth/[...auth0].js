7a05ad2b7cd1660b676297aa43239d2946190489
Cristian Perez <cristian.ap84@gmail.com>
2022-03-29 10:38:01 -0300

    Added error handlers - Updated template.

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,72 @@
-import { handleAuth } from '@auth0/nextjs-auth0'
+import { handleAuth, handleLogin, handleCallback, handleLogout } from '@auth0/nextjs-auth0'
 
-export default handleAuth()
+const buildErrorQueryString = (code, desc) => {
+  let queryString = ''
+  let separator = '?'
+
+  if (code) {
+    queryString = separator + 'code=' + code
+  }
+
+  if (queryString != '') {
+    separator = '&'
+  }
+
+  if (desc) {
+    queryString = queryString + separator + 'desc=' + desc
+  }
+
+  return queryString
+}
+
+const callback = async (req, res) => {
+  try {
+    console.log('Executing callback')
+
+    const afterCallback = (req, res, session, state) => {
+      console.log(state)
+      return session
+    }
+
+    await handleCallback(req, res, { afterCallback })
+  } catch (ex) {
+    console.error('Callback execution failed', { ...ex })
+
+    const { error, error_description } = req.query
+    const queryString = buildErrorQueryString(error, error_description)
+
+    return res.redirect('/error' + queryString)
+  }
+}
+
+const login = async (req, res) => {
+  try {
+    console.log('Executing login')
+
+    await handleLogin(req, res)
+  } catch (ex) {
+    console.error('Login execution failed', { ...ex })
+
+    const { error, error_description } = req.query
+    const queryString = buildErrorQueryString(error, error_description)
+
+    return res.redirect('/error' + queryString)
+  }
+}
+
+const logout = async (req, res) => {
+  try {
+    console.log('Executing logout')
+
+    await handleLogout(req, res)
+  } catch (ex) {
+    console.error('Logout execution failed', { ...ex })
+
+    const { error, error_description } = req.query
+    const queryString = buildErrorQueryString(error, error_description)
+
+    return res.redirect('/error' + queryString)
+  }
+}
+
+export default handleAuth({ callback, login, logout })
